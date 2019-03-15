33ea91095968e16cdc552b40c7fe8e22363a9c03
menanmavericks <menanmavericks@gmail.com>
2019-03-15 11:21:22 +0530

    first

---
+++ twentypro/src/com/twentyprocalc/money.java
@@ -0,0 +1,11 @@
+package com.twentyprocalc;
+
+public class money {
+
+	public static void main(String[] args) {
+		System.out.println("helooooooooo12o");
+		// TODO Auto-generated method stub
+
+	}
+
+}
